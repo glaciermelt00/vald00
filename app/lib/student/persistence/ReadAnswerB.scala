569e3ed91c48a966d152f28d590b6c0dc48bc666
glaciermelt00 <glacier-melt@hotmail.co.jp>
2023-06-19 23:48:25 +0900

    feat: add answer b table files

---
+++ app/lib/student/persistence/ReadAnswerB.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright Jo Harada. All Rights Reserved.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package lib.student.persistence
+
+
+import slick.jdbc.JdbcProfile
+import ixias.persistence.SlickRepository
+import scala.concurrent.Future
+import lib.student.model.ReadAnswer
+
+/**
+ * ReadAnswerB Information
+ */
+case class ReadAnswerBRepository[P <: JdbcProfile]()(implicit val driver: P)
+    extends SlickRepository[ReadAnswer.Id, ReadAnswer, P]
+    with db.SlickResourceProvider[P] {
+  import api._
+
+  // --[ Methods ]--------------------------------------------------------------
+  /**
+   * Get ReadAnswerB data
+   */
+  def get(id: Id): Future[Option[EntityEmbeddedId]] =
+    RunDBAction(ReadAnswerBTable, "slave") { _
+      .filter(_.id === id)
+      .result.headOption
+    }
+
+  /**
+   * Get ReadAnswerB data
+   */
+  def seek(cursor: Cursor): Future[Seq[EntityEmbeddedId]] =
+    RunDBAction(ReadAnswerBTable, "slave") { _
+      .sortBy(_.id.desc)
+      .seek(cursor)
+      .result
+    }
+
+  // --[ Methods ]--------------------------------------------------------------
+  /**
+   * Add ReadAnswerB data
+   */
+  def add(data: EntityWithNoId): Future[Id] =
+    RunDBAction(ReadAnswerBTable) { slick =>
+      slick returning slick.map(_.id) += data.v
+    }
+
+  /**
+   * Update ReadAnswerB data
+   */
+  def update(data: EntityEmbeddedId): Future[Option[EntityEmbeddedId]] =
+    RunDBAction(ReadAnswerBTable) { slick =>
+      val row = slick.filter(_.id === data.id)
+      for {
+        old <- row.result.headOption
+        _   <- old match {
+          case None     => DBIO.successful(0)
+          case Some(id) => row.update(data.v)
+        }
+      } yield old
+    }
+
+  /**
+   * Eliminate specified ReadAnswerB data
+   */
+  def remove(id: Id): Future[Option[EntityEmbeddedId]] =
+    RunDBAction(ReadAnswerBTable) { slick =>
+      val row = slick.filter(_.id === id)
+      for {
+        old <- row.result.headOption
+        _   <- old match {
+          case None    => DBIO.successful(0)
+          case Some(_) => row.delete
+        }
+      } yield old
+    }
+}
